8afa246c40e9cac6bdf88319bb3be58d7c36a7ba
tracer-ics2017 <tracer@njuics.org>
2017-10-04 16:12:33 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:12:33 up 3 days,  5:10,  5 users,  load average: 0.13, 0.15, 0.13 b3694903374196c27644d9de996d21ba34b5466

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,27 +2,25 @@
 
 make_EHelper(add) {
   //TODO();
-  rtl_add(&t2, &id_dest->val, &id_src->val);
-  rtl_get_CF(&t1);
-  rtl_add(&t2, &t2, &t1);
-  operand_write(id_dest, &t2);
-
-  rtl_update_ZFSF(&t2, id_dest->width);
-
-  rtl_sltu(&t0, &t2, &id_dest->val);
-  rtl_set_CF(&t0);
-
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_not(&t0);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
-  rtl_msb(&t0, &t0, id_dest->width);
-   
   print_asm_template2(add);
 }
 
 make_EHelper(sub) {
-  TODO();
+  //TODO();
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
+  rtl_get_CF(&t1);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_sltu(&t0, &id_dest->val, &id_src->val);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template2(sub);
 }
