11f419d4286ddc92edf01dfc674e935544b064ac
tracer-ics2017 <tracer@njuics.org>
2017-10-04 16:53:45 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:53:45 up 3 days,  5:51,  5 users,  load average: 0.31, 0.26, 0.23 ef1c7ae66291527c3790130ffafc23666171524

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,7 +26,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   //TODO();
-	rtl_push(&cpu.eip);
+	rtl_push(&cpu.esp);
 	decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
