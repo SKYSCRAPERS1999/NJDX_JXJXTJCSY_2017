492be936be520094d122469027e33e922dece13b
tracer-ics2017 <tracer@njuics.org>
2017-10-08 20:54:55 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:54:55 up 1 day,  3:47, 10 users,  load average: 0.44, 0.16, 0.11 480b6546ecf2389f7e328bc74baf2ee94b957472

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -14,8 +14,8 @@ make_EHelper(push) {
 make_EHelper(pop) {
   //TODO();
 	rtl_pop(&t0);
+	Log("t0 = %d\n", t0);
 	operand_write(id_dest, &t0);
-	Log("id_dest->val = %d\n", id_dest->val);
   print_asm_template1(pop);
 }
 
