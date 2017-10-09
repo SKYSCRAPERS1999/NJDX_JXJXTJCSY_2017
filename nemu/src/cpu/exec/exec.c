cd287ee0f0bbde09e3738aeb534c921fff888f30
tracer-ics2017 <tracer@njuics.org>
2017-10-09 20:27:06 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:27:06 up  7:44, 10 users,  load average: 0.35, 0.16, 0.11 e7265ddc513f7cad148146f565cd4335cfd537c4

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -185,7 +185,7 @@ opcode_entry opcode_table [512] = {
   /* 0xa8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xac */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xb4 */	EMPTY, EMPTY, IDEX(mov_G2E, movzx), IDEX(mov_G2E, movzx),
+  /* 0xb4 */	EMPTY, EMPTY, IDEXW(mov_G2E, movzx, 1), IDEXW(mov_G2E, movzx, 1),
   /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xbc */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
