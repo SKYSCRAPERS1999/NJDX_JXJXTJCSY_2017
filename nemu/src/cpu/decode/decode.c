5cb1df45e4fd7c7809f21db09a38734430d692ad
tracer-ics2017 <tracer@njuics.org>
2017-10-09 22:21:27 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:21:27 up  9:38, 10 users,  load average: 0.20, 0.12, 0.10 c0281ea38ff3811a29a6676da8106f81ab258123

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -269,7 +269,7 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  Log("id_dest->simm = 0x%x ## *eip = 0x%x\n", id_dest->simm, *eip);
+  //Log("id_dest->simm = 0x%x ## *eip = 0x%x\n", id_dest->simm, *eip);
 	decoding.jmp_eip = id_dest->simm + *eip;
 }
 
