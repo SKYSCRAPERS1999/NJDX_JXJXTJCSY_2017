9cbf627223671e6b049a4d5b217fd4c662d8c0b6
tracer-ics2017 <tracer@njuics.org>
2017-10-09 14:02:37 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:02:37 up  1:20, 10 users,  load average: 0.09, 0.11, 0.10 613caf932613a2d38d08878ea8ad9b6eacb6cf5b

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,7 +38,7 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
-	Log("width = %d ## val = %d\n", op->width, op->val);
+	Log("width = %d ## type = %d\n", op->width, op->type);
   op->simm = instr_fetch(eip, op->width);
 	
   //rtl_li(&op->val, op->simm);
