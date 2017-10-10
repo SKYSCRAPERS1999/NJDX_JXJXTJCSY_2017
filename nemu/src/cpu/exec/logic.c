85655327c35efe576577c505c1ae070b5aad5c1f
tracer-ics2017 <tracer@njuics.org>
2017-10-10 10:54:41 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:54:41 up 22:12, 11 users,  load average: 0.02, 0.11, 0.15 b9d1a65c7129093af93968cc631617c2b9595b56

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -62,7 +62,7 @@ make_EHelper(sar) {
 	t1 = id_dest->val;
 	//t1 >>= id_src->val;
 	rtl_shr(&t1, &t1, &id_src->val);
-	t2 = t1;
+	rtl_sext(&t2, &t1, id_dest->width);
 	operand_write(id_dest, &t2);
 	rtl_update_ZFSF(&id_dest->val, id_dest->width);
   
