79b02959b512df0ebcc7df0bf0cd0e61cc69fee3
tracer-ics2017 <tracer@njuics.org>
2017-10-10 09:02:23 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  09:02:23 up 20:19, 10 users,  load average: 0.74, 0.42, 0.24 5cd6752cf2625ceb45bf2ffc45fcf1e68ec7f5ca

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -127,7 +127,7 @@ make_rtl_setget_eflags(SF)
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
   //TODO();
-	rtl_lm(dest, src1, 4);
+	*dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
