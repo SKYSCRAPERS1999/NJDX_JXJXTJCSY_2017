70b97554eb83ac0ec0b25298ea7da73a89228f1e
tracer-ics2017 <tracer@njuics.org>
2017-10-11 09:34:37 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  09:34:37 up 18:10, 21 users,  load average: 0.34, 0.38, 0.51 1e3cf40488599d3ce2f2b52f3478a3972a9d529f

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -31,7 +31,7 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
 		uint32_t xx = i % _screen.width;
 		if (xx >= x && xx <= x + w && yy >= y && yy <= y + h){
 			//fb[i] = pixels[i];
-			memcpy(fb + i, pixels + i, sizeof(uint32_t));
+			memcpy(fb + i, pixels + i, 4);
 		}
   }
 }
