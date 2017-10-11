27408b690b7b738fa1ba9972d7decba34bd1da78
tracer-ics2017 <tracer@njuics.org>
2017-10-11 14:15:51 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:15:51 up  3:13, 24 users,  load average: 0.08, 0.27, 0.33 654006e05eaf23d29210aaadb1ee6cfea1e3464f

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -58,7 +58,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //TODO();
-	pio_write(id_dest->val, id_src->width, id_src->val);
+	pio_write(id_dest->val, id_dest->width, id_src->val);
 
   print_asm_template2(out);
 
