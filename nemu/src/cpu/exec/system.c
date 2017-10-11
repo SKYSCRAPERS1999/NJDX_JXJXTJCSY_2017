90375131fdb59830d52095066be40f7d8bac9818
tracer-ics2017 <tracer@njuics.org>
2017-10-11 13:56:16 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:56:16 up  2:54, 24 users,  load average: 0.07, 0.24, 0.34 bd7aa2b6fda6debd2cf1c76c2d88982232bc9741

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,7 +46,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   //TODO();
-	t1 = (uint16_t)pio_read(id_src->val, 1);
+	t1 = (uint16_t)pio_read(id_src->val, id_src->width);
 	id_dest->val = t1;
 	operand_write(id_dest, &id_dest->val);
   
