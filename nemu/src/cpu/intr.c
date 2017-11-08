1c245a409df837388fff498f139f82735e3cd05b
tracer-ics2017 <tracer@njuics.org>
2017-11-08 19:49:46 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:49:46 up  6:29, 15 users,  load average: 0.19, 0.16, 0.16 df30d51b5ff6c1bc432587bc9dd0590a911e8a35

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,8 +6,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   //TODO();
+	ret_addr = cpu.eip;
 	rtl_push(&cpu.EFLAGS);
-	rtl_push(&cpu.eip);
+	rtl_push(&ret_addr);
 	uint32_t base = cpu.IDT_base;
 	uint32_t idt_data[2];
 	idt_data[0] = vaddr_read(base + 8 * NO, 4);
