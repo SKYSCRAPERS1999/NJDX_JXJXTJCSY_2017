94890a126987bc9c7c5c2fc998d604c49822ddb3
tracer-ics2017 <tracer@njuics.org>
2017-11-08 23:49:52 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:49:52 up 10:29, 15 users,  load average: 0.29, 0.26, 0.12 2b3f4fc47e22851d423c4163bee1ada916d6e496

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -15,9 +15,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 	idt_data[1] = vaddr_read(base + 8 * NO + 4, 4);
 	uint32_t idta = (idt_data[0] & 0xffff) | (idt_data[1] & 0xffff0000);
 	decoding.jmp_eip = idta;
-	rtl_push(&cpu.EFLAGS);
-	rtl_push(&ret_addr);
-	decoding.is_jmp = 1;
+	//rtl_push(&cpu.EFLAGS);
+	//rtl_push(&ret_addr);
+	//decoding.is_jmp = 1;
 	Log("jmp.eip = 0x%08x\n", decoding.jmp_eip);
 	
 }
