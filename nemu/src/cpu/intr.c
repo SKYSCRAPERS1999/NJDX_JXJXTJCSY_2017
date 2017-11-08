3cd51e06452d4fb7de993e9fade4a57a82a26033
tracer-ics2017 <tracer@njuics.org>
2017-11-08 23:57:59 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:57:59 up 10:37, 15 users,  load average: 0.13, 0.20, 0.14 b6fd2b127f117e5ae4d737ae2d96276be03139ac

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,23 +6,17 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   //TODO();
-	Log("cpu.eip = 0x%08x\n", cpu.eip);
-	Log("ret_addr = 0x%08x\n", ret_addr);
 
 	uint32_t base = cpu.IDT_base;
 	uint32_t idt_data[2];
 	idt_data[0] = vaddr_read(base + 8 * NO, 4);
 	idt_data[1] = vaddr_read(base + 8 * NO + 4, 4);
 	uint32_t idta = (idt_data[0] & 0xffff) | (idt_data[1] & 0xffff0000);
-	cpu.eip = idta;
-	rtl_push(&cpu.EFLAGS);
-	/*
 	decoding.jmp_eip = idta;
+	rtl_push(&cpu.EFLAGS);
 	rtl_push(&ret_addr);
 	decoding.is_jmp = 1;
-	*/
-	Log("jmp.eip = 0x%08x\n", decoding.jmp_eip);
-	
+	Log("ret_addr = 0x%08x\n", ret_addr);
 }
 
 void dev_raise_intr() {
