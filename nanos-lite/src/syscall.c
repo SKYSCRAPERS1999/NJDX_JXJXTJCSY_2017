04ef188a159dbcbd28bd016d14492d442ea5eb1f
tracer-ics2017 <tracer@njuics.org>
2017-11-09 10:55:21 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:55:21 up 21:34, 19 users,  load average: 0.16, 0.09, 0.10 42f47ef867cf5824ac58994a77879ad774cb9f97

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,8 +5,8 @@ _RegSet* do_syscall(_RegSet *r) {
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
-		case 1: {
-			
+		case 0: {
+			SYSCALL_ARG1(r) = 1;	
 			break;
 		}
     default: panic("Unhandled syscall ID = %d", a[0]);
