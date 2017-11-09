191f37f08928cab63bfadfe8475af38b8f41827c
tracer-ics2017 <tracer@njuics.org>
2017-11-09 10:22:50 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:22:50 up 21:02, 19 users,  load average: 0.13, 0.22, 0.28 3a55482914915d7b5b82708b877f8788aebffc84

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,8 @@
 #include "common.h"
-
+_RegSet* do_syscall(_RegSet*);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
+		case 8: do_syscall(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
