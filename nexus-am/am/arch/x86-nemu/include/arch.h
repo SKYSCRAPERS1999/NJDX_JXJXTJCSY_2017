eea403d6710c7b5b46ecb4e4b1f910db0d8d2841
tracer-ics2017 <tracer@njuics.org>
2017-11-09 08:31:18 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  08:31:18 up 19:10, 16 users,  load average: 0.44, 0.42, 0.32 d645056b67427cf9ac7c8a3a1136dd69f7c854b8

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -6,11 +6,17 @@
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PGSIZE    4096    // Bytes mapped by a page
 
+/*
 struct _RegSet {
   uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
   int       irq;
 };
+*/
 
+struct _RegSet {
+  uintptr_t error_code, eflags, cs, eip, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  int       irq;
+};
 #define SYSCALL_ARG1(r) 0
 #define SYSCALL_ARG2(r) 0
 #define SYSCALL_ARG3(r) 0
