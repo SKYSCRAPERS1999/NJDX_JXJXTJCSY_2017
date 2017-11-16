f7802dfc3ba93b579fafd6f6380b0e3f01e59cd2
tracer-ics2017 <tracer@njuics.org>
2017-11-16 16:11:20 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:11:20 up 50 min, 19 users,  load average: 0.09, 0.12, 0.09 30f26c68b6d4e3f1209c1278e8b66a12a1378dbe

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -11,7 +11,7 @@
 
 // FIXME: this is temporary
 
-void _putc(char ch);
+extern void _putc(char ch);
 
 int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
   int ret = -1;
@@ -30,7 +30,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count){
 	if (fd == 1 || fd == 2){
 		for (int i = 0; i < count; i++){
-			_putc((char*)(buf + i));
+			_putc((char)(buf + i));
 		}
 	}
   _exit(SYS_write);
