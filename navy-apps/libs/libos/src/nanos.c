947ece8b6d16222101a80640a0cc1bf20c256173
tracer-ics2017 <tracer@njuics.org>
2017-11-16 16:15:00 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:15:00 up 54 min, 19 users,  load average: 0.11, 0.17, 0.12 43f7d2556b4c6d15c798b7f63eb0659f8f07afe

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -28,11 +28,13 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
+  /*
 	if (fd == 1 || fd == 2){
 		for (int i = 0; i < count; i++){
 			_putc((char)(buf + i));
 		}
 	}
+	*/
   _exit(SYS_write);
 	return count;
 }
