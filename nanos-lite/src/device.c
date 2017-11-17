27d260a19822b407724190049ac66fa9a8ffada8
tracer-ics2017 <tracer@njuics.org>
2017-11-17 21:06:27 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:06:27 up  4:56, 20 users,  load average: 1.05, 0.75, 0.64 51652e933387f2fa5526cfbcdf44cc75aefd8465

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,9 +28,9 @@ void fb_write(const void *buf, off_t offset, size_t len) {
 
 void init_device() {
   _ioe_init();
-  char* tmp = "WIDTH:640\nHEIGHT:480\n";
-	memcpy(dispinfo, tmp, strlen(tmp));
-	//fs_read(FD_DISPINFO, dispinfo, 128);
+  //char* tmp = "WIDTH:640\nHEIGHT:480\n";
+	//memcpy(dispinfo, tmp, strlen(tmp));
+	fs_read(FD_DISPINFO, dispinfo, 128);
 
 	// TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
