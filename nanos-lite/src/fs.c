b843ea8d691cf21e3644ecd3ebb8abd3c6b17124
tracer-ics2017 <tracer@njuics.org>
2017-11-17 15:01:30 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  15:01:30 up 23:40, 20 users,  load average: 1.08, 1.14, 1.11 805dbae7cec5e5e2aa8bed7cec964fdb4ecabab8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,14 +42,18 @@ int fs_open(const char* pathname, int flags, int mode){
 }
 
 int fs_read(int fd, void* buf, size_t len){
-	assert(file_table[fd].open_offset + len <= file_table[fd].size);
+	if (file_table[fd].open_offset + len > file_table[fd].size){
+		len = file_table[fd].size - file_table[fd].open_offset;
+	}
 	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
 	return len;
 }
 
 int fs_write(int fd, void* buf, size_t len){
-	assert(file_table[fd].open_offset + len <= file_table[fd].size);
+	if (file_table[fd].open_offset + len > file_table[fd].size){
+		len = file_table[fd].size - file_table[fd].open_offset;
+	}
 	ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
 	return len;
