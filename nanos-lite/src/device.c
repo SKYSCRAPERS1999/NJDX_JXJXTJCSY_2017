49d8ccc445ee81584cc957ab7a0e5755c7fb8f2f
tracer-ics2017 <tracer@njuics.org>
2017-11-17 20:05:21 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:05:21 up  3:55, 20 users,  load average: 0.50, 0.59, 0.46 525cd28b5e16391a2f2f9ca0cc817d233a9d182

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #define W 400
 #define H 300
+#define FD_DISPINFO 5
 extern uint32_t* const fb;
+extern int fs_read(int, void*, size_t);
 #define NAME(key) \
   [_KEY_##key] = #key,
 
@@ -17,15 +19,18 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+	memcpy(buf, (char*)(dispinfo + offset), len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-	memcpy((uint32_t*)(fb + offset), buf, len);	
+	memcpy((char*)(fb + offset), buf, len);	
 }
 
 void init_device() {
   _ioe_init();
+  fs_read(FD_DISPINFO, dispinfo, 128);
 
-  // TODO: print the string to array `dispinfo` with the format
+	// TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+	
 }
