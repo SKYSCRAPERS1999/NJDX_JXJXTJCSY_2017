2324f1bde048d6f66deb0aa085fa1bab29ead74c
tracer-ics2017 <tracer@njuics.org>
2017-11-17 20:40:27 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:40:27 up  4:30, 20 users,  load average: 0.58, 0.53, 0.58 ed547b39a137edcf619345c79b3bbff7ffff3684

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,7 +32,8 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-	file_table[FD_FB].size = W * H * sizeof(uint32_t) * sizeof(uint32_t);	
+	file_table[FD_FB].size = W * H; 
+	//	* sizeof(uint32_t) * sizeof(uint32_t);	
 	
 }
 
