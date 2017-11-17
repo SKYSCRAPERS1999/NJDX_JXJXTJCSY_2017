c389d87fed739fe8bd28f82d90701b62e3eba5d0
tracer-ics2017 <tracer@njuics.org>
2017-11-17 10:57:16 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:57:16 up 19:36, 22 users,  load average: 1.45, 1.22, 1.21 33d08dd2171f69177d993db2354900c2ee82de

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,7 +37,7 @@ _RegSet* do_syscall(_RegSet *r) {
 		}
 		
 		case SYS_brk: {
-			_heap.end = (void*)SYSCALL_ARG2(r);
+			//_heap.end = (void*)SYSCALL_ARG2(r);
 			Log("_heap.end = %p\n, _end = %p, end = %p\n", _heap.end, &_end, &end);
 			SYSCALL_ARG1(r) = 0;
 			break;
