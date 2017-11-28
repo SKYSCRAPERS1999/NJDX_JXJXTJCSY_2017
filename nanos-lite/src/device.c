6be1f89ed3d090369d765b5d760690881f55edfe
tracer-ics2017 <tracer@njuics.org>
2017-11-29 07:49:19 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  07:49:19 up 10 min, 20 users,  load average: 0.15, 0.19, 0.12 f9eb22ab77e1d593211dd0854738efe64dcb8cd

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,7 +23,7 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-	memcpy((uint32_t*)(fb + offset * 4), buf, len);	
+	memcpy((uint32_t*)(fb + offset / 400), buf, len);	
 }
 
 void init_device() {
