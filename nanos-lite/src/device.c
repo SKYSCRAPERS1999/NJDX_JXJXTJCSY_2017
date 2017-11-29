8205ca736e87752a4cee3db309a406ff5a3a1b83
tracer-ics2017 <tracer@njuics.org>
2017-11-29 10:10:10 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:10:10 up  2:31, 20 users,  load average: 0.08, 0.09, 0.09 4d6fd30d520488a927dbf36929791c2431ec0996

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,7 +16,7 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t len) {
 	int code = _read_key() & 0xff;
-	printf("code = %d\n", code);
+	//printf("code = %d\n", code);
 	if (code != _KEY_NONE){
 		if (code > 0x8000){
 			sprintf(buf, "kd %s\n", keyname[code - 0x8000]);
@@ -28,7 +28,7 @@ size_t events_read(void *buf, size_t len) {
 		unsigned long time = _uptime();
 		sprintf(buf, "t %d\n", time);
 	}
-	printf("buf = %s\n", buf);
+	//printf("buf = %s\n", buf);
 	return strlen(buf);
 }
 
