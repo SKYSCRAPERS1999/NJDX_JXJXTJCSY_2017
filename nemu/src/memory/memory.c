12d877771e8d8001c34ec2fad29d246e33e68c31
tracer-ics2017 <tracer@njuics.org>
2017-12-10 21:20:03 +0800

    >  compile 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:20:03 up 2 days, 22:58, 21 users,  load average: 0.08, 0.35, 0.33 46353b6fe1294e38de4b79976fd04b20281e9363

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -19,8 +19,8 @@ uint32_t page_translate(vaddr_t addr, bool is_write) {
 	uint32_t pde = paddr_read(pde_base + 4 * pde_off, 4);
 	//Log("pde_base = %x\n", pde_base);
 	//Log("addr = %x\n", addr);
-	//Log("pde_off = %x\n", pde_off);
-	Log("pde = %x\n", pde);
+	Log("pde_off = %x\n", pde_off);
+	//Log("pde = %x\n", pde);
 	assert((pde&1) == 1);	
 	
 	uint32_t pte_base = PTE_ADDR(pde);
