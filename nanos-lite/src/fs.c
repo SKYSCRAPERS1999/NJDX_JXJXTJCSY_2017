f0cfee827a01b85f9b89a443fdd8a260a2352921
tracer-ics2017 <tracer@njuics.org>
2017-12-11 16:05:31 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:05:31 up  3:46, 23 users,  load average: 0.24, 0.16, 0.21 242f85c2b990604aee4d69ecf26c67f41afc9583

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,7 +56,7 @@ bool fs_end(int fd){
 }
 
 int fs_read(int fd, void* buf, size_t len){
-	assert(file_table[fd].open_offset <= file_table[fd].size);
+	//assert(file_table[fd].open_offset <= file_table[fd].size);
 	if (file_table[fd].open_offset + len > file_table[fd].size){
 			len = file_table[fd].size - file_table[fd].open_offset;
 	}
