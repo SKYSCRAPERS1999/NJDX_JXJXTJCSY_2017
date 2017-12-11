cfa20c10b795d8b1029d87ce16f00eafdc969ff3
tracer-ics2017 <tracer@njuics.org>
2017-12-11 14:36:56 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:36:56 up  2:18, 23 users,  load average: 0.69, 0.51, 0.37 10d37c4879639865f46ee364b0fae222ef46a8c

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -17,6 +17,7 @@ void load_prog(const char *filename) {
   // TODO: remove the following three lines after you have implemented _umake()
   _switch(&pcb[i].as);
   current = &pcb[i];
+	Log("enter\n");
   ((void (*)(void))entry)();
 
   _Area stack;
