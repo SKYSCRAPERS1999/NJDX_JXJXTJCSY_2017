4ba8904c6aa19f6f647bcf3bfc7b112632b332a5
tracer-ics2017 <tracer@njuics.org>
2017-12-11 16:00:38 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:00:38 up  3:41, 23 users,  load average: 0.14, 0.21, 0.25 fc7508136277a55a34cb52b0d44d67b5a3e8c2d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,14 +14,14 @@ uintptr_t loader(_Protect *as, const char *filename) {
 	//ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
 	Log("ENTER!\n");
 	int fd = fs_open(filename, 0, 0);
-  int N = (2 * fs_filesz(fd) + PGSIZE - 1) / PGSIZE;
-	for (int i = 0; i < N ; i++){
-		void* ENTRY = new_page();
-		_map(as, DEFAULT_ENTRY + i * PGSIZE, ENTRY);
+  //int N = (2 * fs_filesz(fd) + PGSIZE - 1) / PGSIZE;
+	//for (int i = 0; i < N ; i++){
+		//void* ENTRY = new_page();
+		//_map(as, DEFAULT_ENTRY + i * PGSIZE, ENTRY);
 		//Log("ENTRY = 0x%x\n", (uint32_t)ENTRY);
 		//fs_read(fd, ENTRY, fs_filesz(fd));
-		fs_read(fd, ENTRY, PGSIZE);
-	}
+		//fs_read(fd, ENTRY, PGSIZE);
+	//}
 	fs_close(fd);
 	Log("%s loaded\n", filename);
 	return (uintptr_t)DEFAULT_ENTRY;
