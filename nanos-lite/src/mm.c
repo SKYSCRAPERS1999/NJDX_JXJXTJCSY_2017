4db30af32dfdb2a95fdcb9dc8f0a7d4d0434c6d8
tracer-ics2017 <tracer@njuics.org>
2017-12-15 20:05:58 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:05:58 up 1 day, 11:57, 24 users,  load average: 0.56, 0.65, 0.63 d96e51eb7a4189423e278643217df8a8da8eb657

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -18,9 +18,9 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
-	Log("current->cur_brk = 0x%x\n", current->cur_brk);
-	Log("current->max_brk = 0x%x\n", current->max_brk);
-	Log("new_brk = 0x%x\n", new_brk);
+	//Log("current->cur_brk = 0x%x\n", current->cur_brk);
+	//Log("current->max_brk = 0x%x\n", current->max_brk);
+	//Log("new_brk = 0x%x\n", new_brk);
 	if (current->cur_brk == 0){
 		current->cur_brk = current->max_brk = new_brk;
 	}else{
