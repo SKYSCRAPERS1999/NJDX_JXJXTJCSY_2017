c0b806c0761edabdb90b03866ac625f7f7758d42
tracer-ics2017 <tracer@njuics.org>
2017-12-15 17:39:25 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:39:25 up 1 day,  9:31, 24 users,  load average: 0.53, 0.60, 0.68 ff516bbfa978f59b7fb1f2381c8c01cc6f1285d9

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -26,7 +26,7 @@ int mm_brk(uint32_t new_brk) {
 			int N = (new_brk - current->max_brk + PGSIZE - 1)	/ PGSIZE;
 			for (int i = 0; i < N; i++) { 
 				void* pa = new_page();
-				_map(&current->as, (void*)(current->max_brk + i * PGSIZE), pa);
+				_map(current->as.ptr, (void*)(current->max_brk + i * PGSIZE), pa);
 			}
 			current->max_brk = new_brk;
 		}
