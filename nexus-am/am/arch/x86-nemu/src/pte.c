b655bdc00e718f9e705420cf8260fed1ac02b200
tracer-ics2017 <tracer@njuics.org>
2017-12-23 01:15:08 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  01:15:08 up  3:56, 26 users,  load average: 0.05, 0.04, 0.00 337fe31df3ad671c6db48641f6330e7aa10c407b

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -85,10 +85,11 @@ void _unmap(_Protect *p, void *va) {
 
 _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
 	uintptr_t* ST = (uintptr_t*)ustack.start;
+	*ST = *(ST - 4) = *(ST - 8) = 0;
 	ST -= 4 * 4;
-	*(ST - 4) = 0x200;
-	*(ST - 8) = 8;
-	*(ST - 12) = (uintptr_t)entry;
+	*(ST) = 0x200;
+	*(ST - 4) = 8;
+	*(ST - 8) = (uintptr_t)entry;
 	ST -= 14 * 4;
 	return (_RegSet*)ST;
 	//return NULL;
