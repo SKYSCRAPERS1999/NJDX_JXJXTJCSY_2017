15ee02ed7de724060ab6adc7d200beffaf92a189
tracer-ics2017 <tracer@njuics.org>
2017-12-23 01:21:34 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  01:21:34 up  4:02, 26 users,  load average: 0.09, 0.24, 0.11 7666add2de23624b2ec85a4c57da7370c6a08377

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -89,7 +89,7 @@ _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *cons
 	*(ST - 4) = 0x200;
 	*(ST - 8) = 8;
 	*(ST - 12) = (uint32_t)entry;
-	ST -= 12 * 4;
+	ST -= 13 * 4;
 	return (_RegSet*)ST;
 	//return NULL;
 }
