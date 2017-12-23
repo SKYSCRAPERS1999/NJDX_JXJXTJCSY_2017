8867d36d3b9097694e27b2739a22b0b6d42bb5d7
tracer-ics2017 <tracer@njuics.org>
2017-12-23 18:14:35 +0800

    >  run 161220059 lanfangzhou Linux impulse 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  18:14:35 up  5:45, 26 users,  load average: 0.14, 0.18, 0.15 4891ba81b096d22563ccff3dc5b6ed386fe96d5c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,6 +22,7 @@ size_t events_read(void *buf, size_t len) {
 		if (code > 0x8000){
 			sprintf(buf, "kd %s\n", keyname[code - 0x8000]);
 			if (code == _KEY_F12) {
+				printf("_KEY_F12 = %d\n", _KEY_F12);
 				current_game = (current_game == 0 ? 2 : 0);
 			}
 		}else{
